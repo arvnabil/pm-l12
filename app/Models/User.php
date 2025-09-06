d1a2777ed203239c9ac86eb51c13b67a28829ac1
Nabil <nabilstudioapp@gmail.com>
2025-09-06 15:05:29 +0700

    Add Some Notification, and Install Filament v3
    Add Some Notification, and Install Filament

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,11 +3,18 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Ramsey\Uuid\Uuid;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -21,6 +28,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'creation_token',
+        'type',
+        'oidc_username',
+        'email_verified_at',
     ];
 
     /**
@@ -44,5 +55,72 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function (User $item) {
+            if ($item->type == 'db') {
+                $item->password = bcrypt(uniqid());
+                $item->creation_token = Uuid::uuid4()->toString();
+            }
+        });
+
+        static::created(function (User $item) {
+            if ($item->type == 'db') {
+                // $item->notify(new UserCreatedNotification($item));
+            }
+        });
+    }
+
+    public function projectsOwning(): HasMany
+    {
+        return $this->hasMany(Project::class, 'owner_id', 'id');
+    }
+
+    public function projectsAffected(): BelongsToMany
+    {
+        return $this->belongsToMany(Project::class, 'project_users', 'user_id', 'project_id')->withPivot(['role']);
+    }
+
+    public function favoriteProjects(): BelongsToMany
+    {
+        return $this->belongsToMany(Project::class, 'project_favorites', 'user_id', 'project_id');
+    }
+
+    public function ticketsOwned(): HasMany
+    {
+        return $this->hasMany(Ticket::class, 'owner_id', 'id');
+    }
+
+    public function ticketsResponsible(): HasMany
+    {
+        return $this->hasMany(Ticket::class, 'responsible_id', 'id');
+    }
+
+    // public function socials(): HasMany
+    // {
+    //     return $this->hasMany(SocialiteUser::class, 'user_id', 'id');
+    // }
+
+    public function hours(): HasMany
+    {
+        return $this->hasMany(TicketHour::class, 'user_id', 'id');
+    }
+
+    public function totalLoggedInHours(): Attribute
+    {
+        return new Attribute(
+            get: function () {
+                return $this->hours->sum('value');
+            }
+        );
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
     }
 }
